faa7ba9ca6f91047560afd88a09c68a2669478b6
VenuManikanta <mmastervenu18@gmail.com>
2020-07-27 21:26:13 +0530

    first commit

---
+++ shop/views.py
@@ -0,0 +1,29 @@
+
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+from cart.forms import CartAddProductForm
+
+def product_list(request, category_slug=None):
+    """
+    A list of products. This is a very clever view because it can be used
+    both to give a list of all products AND give a list of one category's
+    products. This is b/c category_slug isn't required by default.
+    """
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
